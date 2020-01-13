e6bdbe33faa906afead8cb6c18286f36c156cf46
Intengine <sylwester.pilarz93@gmail.com>
2020-01-13 19:27:35 +0100

    Update Form1.cs

--- YouTubeMP3/Form1.cs
+++ YouTubeMP3/Form1.cs
@@ -42,7 +42,7 @@ namespace YouTubeMP3
 
         private void buttonLogo_Click(object sender, EventArgs e)
         {
-            System.Diagnostics.Process.Start("http://www.intengine.beskidy.pl");
+            System.Diagnostics.Process.Start("https://intengine.beskidy.pl");
         }
     }
 }
